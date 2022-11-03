1f49e7bcedab3dd7b681f01bcd3601a17eacb668
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-11-03 14:47:38 -0500

    Render a few working routes

--- src/app.jsx
+++ src/app.jsx
@@ -1,47 +1,42 @@
 import React from 'react';
-import * as ReactDOMClient from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { Accordian } from './panel-accordian';
-import { SyncInput } from './sync-input';
-import { FilterableList } from './filterable-list';
-import { food_items } from './filter-data';
-import { SearchableProductTable } from './product-table';
-import { product_items } from './product-data';
-import {
-  TickTockClockArrow,
-  TickTockClockFunction,
-  TickTockClockClass
-} from './ticker';
-import { Comment, comment_data } from './comment';
-import { Form, Toggle, LogThisButton } from './forms';
+import { Root } from './routes/root';
+import { Stuff } from './routes/stuff';
+import { About } from './routes/about';
+import { Blog } from './routes/blog';
+import { Contact } from './routes/contact';
+import { ErrorPage } from './error-page';
 
-const { author, comment_text, date_posted } = comment_data;
-const root = ReactDOMClient.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'about',
+        element: <About />
+      },
+      {
+        path: 'blog',
+        element: <Blog />
+      },
+      {
+        path: 'contact',
+        element: <Contact />
+      },
+      {
+        path: 'stuff',
+        element: <Stuff />
+      }
+    ]
+  }
+]);
 
-const App = () => {
-  return (
-    <>
-      <Accordian />
-      <SyncInput />
-      <FilterableList items={food_items} />
-      <SearchableProductTable products={product_items} />
-      <TickTockClockArrow />
-      <TickTockClockFunction />
-      <TickTockClockClass />
-      <Comment
-        author={author}
-        comment_text={comment_text}
-        date_posted={date_posted}
-      />
-      <Form />
-      <Toggle />
-      <LogThisButton />
-    </>
-  );
-};
-
-const refresh = () => {
-  root.render(<App />);
-};
-
-refresh();
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
